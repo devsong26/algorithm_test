492f7766166e7c9b477d1de3eabb7a7c175a4b09
devsong26 <devsong26@gmail.com>
2022-03-05 12:07:59 +0900

    프로그래머스 - 실패율 성공

--- src/programmers/FailureRate.java
+++ src/programmers/FailureRate.java
@@ -3,8 +3,8 @@ package programmers;
 public class FailureRate {
 
     public static void main(String[] args){
-        int n = 4;
-        int[] stages = {4,4,4,4,4};
+        int n = 10;
+        int[] stages = {5,5,5,5,5};
 
         Solution s = new Solution();
         int[] result = s.solution(n, stages);
@@ -21,44 +21,30 @@ public class FailureRate {
                     if(stages[j] == i) remainCnt++;
                 }
 
-                failureRate[i - 1] = (double) remainCnt / (double) playerCnt;
+                failureRate[i - 1] = (double) remainCnt / (double) (playerCnt == 0 ? 1 : playerCnt);
             }
 
-            int[] answer = new int[N], subStages;
-            double maxRate = 0.0;
-            for(int i=0, isAllCheck = 0; isAllCheck < N;){
-                isAllCheck = 0;
+            double maxRate = -1.0, sum = 0.0;
+            int[] answer = new int[N];
+            int i = 0;
+
+            while(sum > (-2.0*N)){
+                sum = 0;
                 for(int j=0; j<failureRate.length; j++){
                     if(maxRate < failureRate[j]) maxRate = failureRate[j];
                 }
 
-                subStages = new int[N];
                 for(int j=0; j<failureRate.length; j++){
                     if(maxRate == failureRate[j]){
-                        subStages[j] = (j + 1);
-                        failureRate[j] = -1.0;
+                        answer[i++] = (j + 1);
+                        failureRate[j] = -2.0;
                     }
                 }
 
-                for(int j = 0; j < subStages.length; j++){
-                    for(int k = j + 1; k<subStages.length; k++){
-                        if(subStages[j] > subStages[k]){
-                            int temp = subStages[k];
-                            subStages[k] = subStages[j];
-                            subStages[j] = temp;
-                        }
-                    }
-                }
+                for(int j=0; j<failureRate.length; j++)
+                    sum += failureRate[j];
 
-
-                for (int n : subStages){
-                    if(n > 0) answer[i++] = n;
-                }
-                maxRate = 0.0;
-
-                for(int n : answer){
-                    if(n > 0) isAllCheck++;
-                }
+                maxRate = -1.0;
             }
 
             return answer;
