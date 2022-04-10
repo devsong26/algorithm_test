014dd45b5b628b1edaed82c8324deda0d6b8a236
devsong26 <devsong26@gmail.com>
2022-04-10 22:19:38 +0900

    프로그래머스 - 모의고사 완료

--- src/programmers/GiveUpMath.java
+++ src/programmers/GiveUpMath.java
@@ -1,54 +1,37 @@
 package programmers;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GiveUpMath {
 
     public static void main(String[] args){
         int[] answers = {1,2,3,4,5};
 
         Solution s = new Solution();
-        s.solution(answers);
+        int[] result = s.solution(answers);
     }
 
     static class Solution {
         public int[] solution(int[] answers) {
+            Map<Integer, Integer> results = new HashMap<>();
             int[][] checks = {
                     {1, 2, 3, 4, 5},
                     {2, 1, 2, 3, 2, 4, 2, 5},
                     {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-            int[][] matchList = new int[checks.length][2];
-            int positiveNumCnt = 0;
 
-            //입력
             for(int i=0; i<checks.length; i++){
-                int matchCnt = getMatchCnt(answers, checks[i]);
-                if(matchCnt == 0) continue;
-
-                matchList[i][0] = (i + 1);
-                matchList[i][1] = matchCnt;
-                positiveNumCnt++;
+                results.put((i + 1), getMatchCnt(answers, checks[i]));
             }
 
-            //정렬
-            for(int i=0, j=0; i<matchList.length; i++){
-                if(matchList[i][1] > matchList[j][1]){
-                    int[] temp = matchList[i];
-                    matchList[i] = matchList[j];
-                    matchList[j] = temp;
-                }
-
-                j++;
-                if(j == matchList.length - 1){
-                    i++;
-                    j = 0;
-                }
-            }
-
-            int[] results = new int[positiveNumCnt];
-            for(int i=0; i<matchList.length; i++){
-                if(matchList[i][1] > 0) results[i] = matchList[i][0];
-            }
-
-            return results;
+            final int maxCnt = Math.max(results.get(1), Math.max(results.get(2), results.get(3)));
+            return results.entrySet()
+                    .stream()
+                    .filter(e -> e.getValue() >= maxCnt)
+                    .sorted(Comparator.comparingInt(Map.Entry::getKey))
+                    .mapToInt(Map.Entry::getKey)
+                    .toArray();
         }
 
         private int getMatchCnt(int[] answers, int[] checks){
