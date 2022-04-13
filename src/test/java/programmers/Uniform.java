cffb2246221413722ca7e98e3852fda6231a7fda
devsong26 <devsong26@gmail.com>
2022-04-13 23:29:22 +0900

    체육복 문제풀이 - 2 / 20케이스 중 2개 틀림

--- src/test/java/programmers/Uniform.java
+++ src/test/java/programmers/Uniform.java
@@ -1,36 +1,83 @@
 package programmers;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Uniform {
+    /**
+     * ##### 테스트 케이스에서 알아서 넣어줌 #####
+     * 전체 학생의 수는 2명 이상 30명 이하입니다.
+     * 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+     * 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+     * ########################################
+     *
+     *
+     * 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+     *
+     * 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다.
+     * 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며,
+     * 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+     */
+
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> reserves =
-                Arrays.stream(reserve)
-                        .filter(e -> {
-                            boolean notContain = true;
-                            for(int l : lost){
-                                if(l == e) notContain = false;
-                            }
-                            return notContain;
-                        })
-                        .boxed()
-                        .collect(Collectors.toList());
+        return n - lost.length
+                + getLostReserveCnt(lost, reserve)
+                + getBorrowerCnt(lost, reserve);
+    }
 
-        int result = n - lost.length;
+    private int getLostReserveCnt(int[] lost, int[] reserve){
+        int lostReserveCnt = 0;
 
-        for(int l : lost){
-            Integer after = (l + 1);
-            Integer before = (l - 1);
-            if(reserves.contains(before)){
-                result++;
-                reserves.remove(before);
-            }else if(reserves.contains(after)){
-                result++;
-                reserves.remove(after);
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    lost[i] = 0;
+                    reserve[j] = 0;
+                    lostReserveCnt++;
+                    break;
+                }
             }
         }
 
-        return result;
+        return lostReserveCnt;
     }
+
+    private int getBorrowerCnt(int[] lost, int[] reserve) {
+        int borrowCnt = 0;
+        List<Integer> reserveList =
+                Arrays.stream(reserve)
+                        .filter(e -> e > 0)
+                        .boxed()
+                        .collect(Collectors.toList());
+
+        for(Integer l : lost){
+            if(l == 0) continue;
+
+            Integer pre = l - 1;
+            if(pre > 0){
+                if(removeIfContains(pre, reserveList)){
+                    borrowCnt++;
+                    continue;
+                }
+            }
+
+            Integer post = l + 1;
+            if(removeIfContains(post, reserveList)){
+                borrowCnt++;
+                continue;
+            }
+        }
+
+        return borrowCnt;
+    }
+
+    private boolean removeIfContains(Integer i, List<Integer> list){
+        if(list.contains(i)){
+            list.remove(i);
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/programmers/UniformTest.java
+++ src/test/java/programmers/UniformTest.java
@@ -30,4 +30,23 @@ public class UniformTest {
         Uniform uniform = new Uniform();
         uniform.solution(5, lost, reserve);
     }
+
+//    @Test
+//    void getLostReserveCnt() {
+//        int[] lost = {1, 2, 4}; //체육복을 도난 당한 학생들
+//        int[] reserve = {1, 3, 5}; //여벌의 체육복을 가진 학생수
+//
+//        Uniform uniform = new Uniform();
+//        System.out.println(uniform.getLostReserveCnt(lost, reserve));
+//        System.out.println();
+//    }
+//
+//    @Test
+//    void getBorrowerCnt(){
+//        int[] lost = {0, 2, 4}; //체육복을 도난 당한 학생들
+//        int[] reserve = {0, 3, 5}; //여벌의 체육복을 가진 학생수
+//
+//        Uniform uniform = new Uniform();
+//        Assertions.assertEquals(uniform.getBorrowerCnt(lost, reserve), 2);
+//    }
 }
