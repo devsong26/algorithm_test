c4a3733f21d177b0d51cb4b249b6c7823399e0cd
devsong26 <devsong26@gmail.com>
2022-04-16 14:53:46 +0900

    약수의 개수와 덧셈

---
+++ src/test/java/programmers/MeasureTest.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MeasureTest {
+
+    private Measure measure;
+
+    @BeforeEach
+    public void setUp(){
+        measure = new Measure();
+    }
+
+    @Test
+    void 입력_1() {
+        int left = 13;
+        int right = 17;
+        assertEquals(43, measure.solution(left, right));
+    }
+
+    @Test
+    void 입력_2() {
+        int left = 24;
+        int right = 27;
+        assertEquals(52, measure.solution(left, right));
+    }
+
+    @Test
+    void 제곱수_확인() {
+        Double sqrt = Math.sqrt(24);
+        System.out.println((sqrt - sqrt.intValue()));
+    }
+}
