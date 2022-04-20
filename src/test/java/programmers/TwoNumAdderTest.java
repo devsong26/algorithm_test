308fc5a1d1e3d5c91f68bb5d0600645440b6ba35
devsong26 <devsong26@gmail.com>
2022-04-20 23:05:59 +0900

    두수 뽑아 더하기

---
+++ src/test/java/programmers/TwoNumAdderTest.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TwoNumAdderTest {
+
+    private TwoNumAdder adder;
+
+    @BeforeEach
+    public void setUp(){
+        adder = new TwoNumAdder();
+    }
+
+    @Test
+    public void 입력_1(){
+        int[] numbers = {2,1,3,4,1};
+        int[] result = adder.solution(numbers);
+        int[] expected = {2,3,4,5,6,7};
+
+        for(int i=0; i< result.length; i++){
+            assertEquals(expected[i], result[i]);
+        }
+    }
+
+    @Test
+    public void 입력_2(){
+        int[] numbers = {5,0,2,7};
+        int[] result = adder.solution(numbers);
+        int[] expected = {2,5,7,9,12};
+
+        for(int i=0; i< result.length; i++){
+            assertEquals(expected[i], result[i]);
+        }
+    }
+
+}
