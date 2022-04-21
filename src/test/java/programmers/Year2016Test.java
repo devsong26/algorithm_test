436209f69e066028912929c97a2ba3f0fff25482
devsong26 <devsong26@gmail.com>
2022-04-21 22:12:55 +0900

    2016년

---
+++ src/test/java/programmers/Year2016Test.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Year2016Test {
+
+    private Year2016 year2016;
+
+    @BeforeEach
+    public void setUp(){
+        year2016 = new Year2016();
+    }
+
+    @Test
+    void 입력_1() {
+        assertEquals(year2016.solution(5, 24), "TUE");
+    }
+
+}
