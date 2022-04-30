5dc2b3071a8d8b2b2cc6d2d23ca4f5769c9e386d
devsong26 <devsong26@gmail.com>
2022-04-30 14:34:56 +0900

    두 정수 사이의 합

---
+++ src/test/java/programmers/SumBoundaryNumberTest.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumBoundaryNumberTest {
+
+    private SumBoundaryNumber sumBoundaryNumber;
+
+    @BeforeEach
+    public void setUp(){
+        sumBoundaryNumber = new SumBoundaryNumber();
+    }
+
+    @Test
+    public void 입력_1(){
+        assertEquals(12, sumBoundaryNumber.solution(3, 5));
+    }
+
+    @Test
+    public void 입력_2(){
+        assertEquals(3, sumBoundaryNumber.solution(3, 3));
+    }
+
+    @Test
+    public void 입력_3(){
+        assertEquals(12, sumBoundaryNumber.solution(5, 3));
+    }
+
+    @Test
+    public void 입력_4(){
+        assertEquals(0, sumBoundaryNumber.solution(
+                -10000000, 10000000));
+    }
+
+}
