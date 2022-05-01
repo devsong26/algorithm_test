3728674bb6c701a818ba046b22a068a658ac2308
devsong26 <devsong26@gmail.com>
2022-05-01 21:33:00 +0900

    짝수와 홀수

---
+++ src/test/java/programmers/EvenAndOddTest.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EvenAndOddTest {
+
+    private EvenAndOdd evenAndOdd;
+
+    @BeforeEach
+    public void setUp(){
+        evenAndOdd = new EvenAndOdd();
+    }
+
+    @Test
+    public void 입력_1(){
+        assertEquals("Odd", evenAndOdd.solution(3));
+    }
+
+    @Test
+    public void 입력_2(){
+        assertEquals("Even", evenAndOdd.solution(4));
+    }
+
+}
