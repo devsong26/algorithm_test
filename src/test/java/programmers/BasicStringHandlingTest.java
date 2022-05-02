641d442e7c32981fb14a2a438bf06f167ef6a16d
devsong26 <devsong26@gmail.com>
2022-05-02 21:49:55 +0900

    문자열 다루기 기본

---
+++ src/test/java/programmers/BasicStringHandlingTest.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicStringHandlingTest {
+
+    private BasicStringHandling basicStringHandling;
+
+    @BeforeEach
+    public void setUp(){
+        basicStringHandling = new BasicStringHandling();
+    }
+
+    @Test
+    void 입력_1() {
+        assertFalse(basicStringHandling.solution("a234"));
+    }
+
+    @Test
+    void 입력_2() {
+        assertTrue(basicStringHandling.solution("1234"));
+    }
+
+    @Test
+    void 입력_3() {
+        assertTrue(basicStringHandling.solution("0"));
+    }
+
+    @Test
+    void 입력_4() {
+        assertFalse(basicStringHandling.solution("O"));
+    }
+
+    @Test
+    void 입력_5() {
+        assertTrue(basicStringHandling.solution("01234567"));
+    }
+}
