68ef856e70a5508cafc90dcd1f28c96d804f0ec6
devsong26 <devsong26@gmail.com>
2022-07-05 20:00:27 +0900

    문자열 내 p와 y의 개수

---
+++ src/main/java/programmers/PYNumbers.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class PYNumbers {
+
+    public static void main(String[] args){
+        new Solution().solution("pPoooyY");
+    }
+
+    static class Solution {
+        boolean solution(String s) {
+            int pCnt = 0, yCnt = 0;
+
+            for(char ch : s.toLowerCase().toCharArray()){
+                if(ch == 'p') pCnt++;
+                else if(ch == 'y') yCnt++;
+            }
+
+            return pCnt == yCnt;
+        }
+    }
+
+}
